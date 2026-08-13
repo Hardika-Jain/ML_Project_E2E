bc66ec7dc2e0f7a659dae908d6294b1d4f78e25a
Hardika Jain <hardikajain@Hardikas-MacBook-Air.local>
2026-08-12 20:59:39 -0700

    setup commit #1

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from typing import List
+'''Reading the requirements.txt file and returning the list of requirements'''
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='ML_Project_Hardika',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
